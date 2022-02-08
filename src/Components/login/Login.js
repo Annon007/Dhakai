b6856d38ad6938a2be209db4f59e3e50fb5609ae
Annon007 <62867777+Annon007@users.noreply.github.com>
2022-02-09 04:42:53 +0600

    Dhakai Assingment

--- src/Components/login/Login.js
+++ src/Components/login/Login.js
@@ -2,12 +2,13 @@ import React from "react";
 import styles from "./login.module.css";
 import Modal from "../../Ui/Modal";
 // import { getLogin } from "../../Api/login";
-const getLogin = async data => {
+const getLogin = async (data, key) => {
     try {
         const sendReq = fetch("https://devapi.dhakai.com/api/v2/login-buyer", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                "Authorization": key,
             }, body: JSON.stringify(data)
         })
         const res = await sendReq;
@@ -17,13 +18,27 @@ const getLogin = async data => {
 
     }
 };
+const getApiKey = async () => {
+    try {
+        const sendAPIreq = fetch("https://devapi.dhakai.com/api/v2/deviceuid");
+        const res = await sendAPIreq;
+        const response = await res.json();
+        return response.result.deviceUuid;
+    } catch (err) { }
+
+}
 const Login = () => {
-    const handleFormSubmit = e => {
+    const handleFormSubmit = async e => {
         e.preventDefault();
         const formSubmit = new FormData(e.target);
         const data = Object.fromEntries(formSubmit);
-        console.log(data);
-        getLogin(data);
+        const apikey = await getApiKey();
+        console.log(apikey)
+        getLogin(data, apikey);
+        // console.log({...data,});
+        // const convert = uuidAPIKey.toAPIKey(`${apikey}`);
+        // console.log(convert, "API KEY")
+        // getLogin(data);
     }
     return <Modal>
         <form onSubmit={handleFormSubmit}>
